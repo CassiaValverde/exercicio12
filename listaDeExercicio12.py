6cd1c85e6ae445fb6f4a0c9b5df76a57fab58a26
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-05 23:19:19 -0300

    Add files via upload

---
+++ listaDeExercicio12.py
@@ -0,0 +1,5 @@
+#calcular a área de um triângulo
+n1 = float(input("Informe o valor da base "))
+n2 = float(input("Informe o valor da altura "))
+calculo = (n1*n2)/2
+print("A área desse triângulo é ",calculo)
